acb6f289af640f686561998d639d4059b10e41d7
singh-that-codes <500091028@stu.upes.ac.in>
2023-12-10 23:28:44 +0530

    ading files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React and CSS modules
+import React, { useState } from 'react';
+import './App.css'; // Create an App.css file for styling
 
-function App() {
+// Functional component App
+const App = () => {
+  // State for input value and output
+  const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
+
+  // Function to handle input change
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Function to handle form submission
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // You can perform any processing on the inputValue here
+    // For now, let's just set the output to the reversed input
+    setOutputValue(inputValue.split('').reverse().join(''));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>React String Processor</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="inputField">Enter a String:</label>
+        <input
+          type="text"
+          id="inputField"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Type something..."
+        />
+        <button type="submit">Process</button>
+      </form>
+      {outputValue && (
+        <div className="output-container">
+          <h2>Processed String:</h2>
+          <p>{outputValue}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
